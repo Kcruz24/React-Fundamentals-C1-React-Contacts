2485f4c8a23aed374218a4136e121168527b4e9b
Kevin O. Cruz <kocruz.24@gmail.com>
2024-09-13 14:57:44 -0400

    add App.js latest update

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,41 @@
+import { useState } from 'react';
 import '../css/App.css';
 import ListContacts from './ListContacts';
 
 const App = () => {
-	const contacts = [
+	const initialContacts = [
 		{
 			id: 'karen',
 			name: 'Karen Isgrigg',
-			handle: 'karen_isgrigg',
+			handle: '@karen_isgrigg',
 			avatarURL: 'http://localhost:5001/karen.jpg',
 		},
 		{
 			id: 'richard',
 			name: 'Richard Kalehoff',
-			handle: 'richardkalehoff',
+			handle: '@richardkalehoff',
 			avatarURL: 'http://localhost:5001/richard.jpg',
 		},
 		{
 			id: 'tyler',
 			name: 'Tyler McGinnis',
-			handle: 'tylermcginnis',
+			handle: '@tylermcginnis',
 			avatarURL: 'http://localhost:5001/tyler.jpg',
 		},
 	];
 
+	const [contacts, setContacts] = useState(initialContacts);
+
+	const removeContactHandler = (contact) => {
+		setContacts(contacts.filter((c) => c.id !== contact.id));
+	};
+
 	return (
 		<>
-			<ListContacts contacts={contacts} />
+			<ListContacts
+				contacts={contacts}
+				onDeleteContact={removeContactHandler}
+			/>
 		</>
 	);
 };
